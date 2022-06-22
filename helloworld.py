aa9e37df06fa9e3e69f24f6bb06f1451f08a6461
thuyphanthihoai <88868249+thuyphanthihoai@users.noreply.github.com>
2022-06-22 21:07:49 +0700

    hello

---
+++ helloworld.py
@@ -0,0 +1,14 @@
+a = "helloword"
+print(a)
+for x in "pokemon":
+  print(x)
+b: str = "hello python"
+print(b[3:])
+b: str = "hello python"
+print(b[5:])
+b: str = "hello python"
+print(b[-5:-2])
+b: str = "HELLO WORLD"
+print(b.lower())
+m = [3,4,6,7,2,5,2,1]
+print(type(m))
